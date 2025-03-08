571c24b71d32be48263704f98957cc922cd26f1b
pouriashiralipour <pouriashiralipour@gmail.com>
2025-03-08 17:28:59 +0330

    create cryptoApi for adding api to it

--- src/components/services/cryptoApi.js
+++ src/components/services/cryptoApi.js
@@ -4,3 +4,5 @@ const API_KEY = "CG-NAgBbqjum7yU19PUkH81vFLT";
 const getCoinList = () => {
   return `${BASE_URL}/coins/markets?vs_currency=usd&per_page=20&page=1&x_cg_demo_api_key=${API_KEY}`;
 };
+
+export { getCoinList };
